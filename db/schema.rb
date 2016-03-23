71cf2274e6c7b0babe33aa42dac5088edd09e5f6
Roger Crist <roger.crist@rockyou.com>
2016-03-22 17:20:51 -0700

    a few tweaks . . . added username: user to the database.yml file and a couple of other odds and ends

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160318222201) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "terrains", force: :cascade do |t|
     t.string   "name"
     t.integer  "art_index"
